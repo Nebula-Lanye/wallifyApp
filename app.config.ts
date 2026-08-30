cfa80a10312e263fdbb47250ca3637145ecaee93
Nebula-Lanye <lkr2312@163.com>
2026-08-30 13:16:42 +0800

    更新 app.config.ts
    更新版本号

--- app.config.ts
+++ app.config.ts
@@ -41,7 +41,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: "1.7.405",
+  version: "1.7.505-beta",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
